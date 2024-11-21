e7a3f468ac43bd0e843b2721b8453efa331c1b33
Niklas Röttger <niklas.roettger@outlook.de>
2024-11-21 11:09:09 +0100

    add session code

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import "./styles.css";
 import Movie from "./components/Movie";
 import Form from "./components/Form";
+import { uid } from "uid";
 
 const initialMovieData = [
   {
@@ -24,17 +25,38 @@ const initialMovieData = [
 export default function App() {
   const [movies, setMovies] = useState(initialMovieData);
 
+  function handleAddMovie(newMovie) {
+    setMovies([...movies, { id: uid(), ...newMovie }]);
+  }
+
+  function handleDeleteMovie(idToDelete) {
+    setMovies(movies.filter((movie) => movie.id !== idToDelete));
+  }
+
+  function handleToggleLike(idToToggle) {
+    const updatedMovies = movies.map((movie) =>
+      movie.id === idToToggle ? { ...movie, isLiked: !movie.isLiked } : movie
+    );
+    setMovies(updatedMovies);
+  }
+
   return (
     <div className="app">
       <h1>Favorite Movies</h1>
       <ul className="list">
         {movies.map((movie) => (
           <li key={movie.id}>
-            <Movie name={movie.name} isLiked={movie.isLiked} />
+            <Movie
+              name={movie.name}
+              isLiked={movie.isLiked}
+              onDeleteMovie={handleDeleteMovie}
+              onToggleLike={handleToggleLike}
+              id={movie.id}
+            />
           </li>
         ))}
       </ul>
-      <Form />
+      <Form onAddMovie={handleAddMovie} />
     </div>
   );
 }
--- src/components/Form/index.jsx
+++ src/components/Form/index.jsx
@@ -1,12 +1,21 @@
 import "./Form.css";
 
-export default function Form() {
+export default function Form({ onAddMovie }) {
   function handleSubmit(event) {
     event.preventDefault();
     const formData = new FormData(event.target);
     const data = Object.fromEntries(formData);
+    console.log("daten aus dem Formular: ", data);
 
-    console.log(data);
+    onAddMovie(data);
+
+    // Alternativer Weg, um an die Daten zu kommen:
+
+    // const name = event.target.elements.name.value
+    // const isLiked = event.target.elements.isLiked.checked
+    // const data = { name: name, isLiked: isLiked }
+    // onAddMovie(data)
+
     event.target.reset();
     event.target.elements.name.focus();
   }
